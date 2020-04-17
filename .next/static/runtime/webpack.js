fa8384c835875487f18d3fd22bf730579459a63d
Alla Yakubova <yakubova92@gmail.com>
2020-04-17 00:50:53 -0400

    reorganized front end files

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7bbb920b5eea32c98d4c";
+/******/ 	var hotCurrentHash = "67282594473248f29915";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
