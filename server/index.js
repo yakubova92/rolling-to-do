ddbb2bd22d7880f167bb1ac97aa2512300fe6b91
Alla Yakubova <yakubova92@gmail.com>
2020-04-25 19:26:39 -0400

    express server listening

---
+++ server/index.js
@@ -0,0 +1,29 @@
+ const express = require('express')
+ const next = require('next')
+
+ const PORT = process.env.PORT || 3000
+ const dev = process.env.NODE_ENV !== 'production'
+ const app = next({ dev })
+ const handle = app.getRequestHandler()
+
+ app.prepare()
+  .then( () => {
+    const server = express()
+
+    // all routes not handled by express will be handled by next
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(PORT, err => {
+      if (err) {
+        throw err
+      }else {
+        console.log(`Server listening at port ${PORT}`)
+      }
+    })
+  })
+  .catch(ex => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
