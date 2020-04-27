065a40ce3c0a64b0907ae3c75515a1e0e66f7f94
Alla Yakubova <yakubova92@gmail.com>
2020-04-26 22:31:15 -0400

    set up graphQL

--- server/index.js
+++ server/index.js
@@ -1,17 +1,63 @@
  const express = require('express')
  const next = require('next')
+ const bodyParser = require('body-parser')
+ const graphqlHttp = require('express-graphql')
+ const { buildSchema } = require('graphql')
 
  const PORT = process.env.PORT || 3000
  const dev = process.env.NODE_ENV !== 'production'
  const app = next({ dev })
  const handle = app.getRequestHandler()
 
+ const events = []
+
  app.prepare()
   .then( () => {
     const server = express()
 
-    const weekRoutes = require('./routes/index.js')
-    server.use('/api', weekRoutes)
+    server.use(bodyParser.json())
+
+    server.use('/graphql', graphqlHttp({
+      schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          date: String!
+        }
+        input EventInput {
+          title: String!
+          description: String!
+          date: String!
+        }
+        type RootQuery {
+          events: [Event!]!
+        }
+        type RootMutation {
+          createEvent(eventInput: EventInput): Event
+        }
+        schema {
+          query: RootQuery
+          mutation: RootMutation
+        }
+      `),
+      rootValue: {
+        events: () => {
+          return events
+        },
+        createEvent: args => {
+          const event = {
+            _id: Math.random().toString(),
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            date: args.eventInput.date
+          }
+          events.push(event)
+          return event
+        }
+      },
+      graphiql: true
+    }))
 
     // all routes not handled by express will be handled by next
     server.get('*', (req, res) => {
--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/week', (req, res) => {
-  res.end('Week route')
-})
-
-module.exports = router
